5c82230cc0ee75eb8607417eda2e9c7020d0ce72
lohith-pras <loh.t.prasanna@fau.de>
2026-05-19 18:39:28 +0200

    refactor: remove TopNavBar from Layout and adjust main content padding for safe area insets

---
+++ ios/Nest/Nest/NestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestApp.swift
+//  Nest
+//
+//  Created by Lohith Tarikere Prasanna on 19.05.26.
+//
+
+import SwiftUI
+
+@main
+struct NestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
